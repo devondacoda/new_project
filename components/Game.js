365605a4277a168cc2e7d3c5538baa361bbbcf30
Devon JohnBaptiste <devondevon@outlook.com>
2017-12-18 15:13:24 -0500

    Added functionality to game component

--- components/Game.js
+++ components/Game.js
@@ -1,12 +1,74 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+// import { StackNavigator } from 'react-navigation';
+import { emojis } from '../utils/emoji';
 
-const Game = () => (
-  <View style={styles.container}>
-    <Text>This is my Game Component!</Text>
-  </View>
-);
+class Game extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      emojiArr: [],
+      randomQuestion: 0,
+      score: 0,
+      guess: '',
+    };
+
+    this.restartGame = this.restartGame.bind(this);
+    this.pickRandomQuestion = this.pickRandomQuestion.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleInputSubmit = this.handleInputSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.restartGame();
+  }
+
+  restartGame() {
+    const emojiArr = emojis.slice();
+    const randomQuestion = this.pickRandomQuestion(emojis);
+    this.setState({ emojiArr, randomQuestion, score: 0 });
+  }
+
+  pickRandomQuestion(questionsArr) {
+    return Math.floor(Math.random() * questionsArr.length);
+  }
+
+  handleInputChange(guess) {
+    this.setState({ guess });
+  }
+
+  handleInputSubmit() {
+    this.setState({ guess: '' });
+  }
+
+  render() {
+    const {
+      score, emojiArr, randomQuestion, guess,
+    } = this.state;
+    return (
+      <View style={styles.container}>
+        <Text>Score: {score}</Text>
+        <Text>
+        {
+          emojiArr.length
+          && emojiArr[randomQuestion].question
+        }
+        </Text>
+        <TextInput
+          onChangeText={this.handleInputChange}
+          value={guess}
+          placeholder="Guess the phrase!" />
+        <Button
+          title="Submit"
+          onPress={this.handleInputSubmit} />
+        <Button
+          title="Restart Game"
+          onPress={this.restartGame} />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
